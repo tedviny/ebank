614f5fa5affa33be641d6be12ec76cec28edb6f4
Ted Loungou <loungouviny15@gmail.com>
2025-01-07 22:02:29 +0100

    add envs

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,7 @@ const cors = require('cors');
 const app = express();
 
 const corsOptions = {
-    origin: 'http://frontend:4000',
+    origin: process.env.FRONTEND_URL || 'http://localhost:4000',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
     optionsSuccessStatus: 204,
@@ -21,4 +21,9 @@ app.use('/api/auth', authRoutes);
 app.use('/api/home', dashboardRoutes);
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+const HOST = '0.0.0.0';
+
+app.listen(PORT, HOST, () => {
+  console.log(`Server running on port ${PORT}`);
+  console.log(`Backend accessible at http://localhost:${PORT}`);
+});
